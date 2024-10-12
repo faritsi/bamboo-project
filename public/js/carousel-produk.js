a916caee9d749d7d24573dae34ecefc06f4521ca
Fazry Javier Nugraha <fazryjavier125@gmail.com>
2024-10-12 12:27:31 +0700

    responsive service, pimpinan, produk (but still onprogress)

--- public/js/carousel-produk.js
+++ public/js/carousel-produk.js
@@ -1,3 +1,31 @@
+// // Carousel
+// let slideIndex = 0;
+// const slides = document.querySelectorAll(".carousel-slide");
+// const totalSlides = slides.length;
+// const track = document.querySelector(".carousel-track");
+
+// function showSlide(index) {
+//     const offset = -(index * 100) / 3; // Menggerakkan track sebesar 1/3 dari lebar setiap slide
+//     track.style.transform = `translateX(${offset}%)`;
+// }
+
+// function nextSlide() {
+//     slideIndex = (slideIndex + 1) % (totalSlides - 2); // Kurangi 2 karena kita menampilkan 3 produk sekaligus
+//     showSlide(slideIndex);
+// }
+
+// function prevSlide() {
+//     slideIndex = (slideIndex - 1 + totalSlides - 2) % (totalSlides - 2);
+//     showSlide(slideIndex);
+// }
+
+// // Awal menampilkan slide pertama
+// showSlide(slideIndex);
+
+// // Geser otomatis setiap 5 detik
+// setInterval(nextSlide, 5000);
+
+
 let slideIndex = 0;
 const slides = document.querySelectorAll(".carousel-slide");
 const totalSlides = slides.length;
